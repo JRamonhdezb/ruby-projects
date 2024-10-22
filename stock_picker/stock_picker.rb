96c3c6eb7a154c2b7a2eade01de23bb8250599aa
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-10-22 10:43:18 -0700

    Auto correct code by using rubocop gem

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,22 +1,19 @@
-
-
 def stock_picker(array)
   arr = []
   profit = 0
   array.each_with_index do |i_value, i_index|
     array.each_with_index do |j_value, j_index|
-      if (j_value >= i_value) && (j_index >= i_index)
-        result = j_value - i_value
-        if result > profit
-          profit = result
-          arr[0] = i_index && arr[1] = j_index
-        end
+      next unless (j_value >= i_value) && (j_index >= i_index)
+
+      result = j_value - i_value
+      if result > profit
+        profit = result
+        arr[0] = i_index && arr[1] = j_index
       end
     end
   end
   puts profit
   p arr
-
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
