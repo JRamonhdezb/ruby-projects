ff53f4da7831d2ee76a5d436ccd29d129eeccde5
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-10-22 10:13:47 -0700

    Auto correct code by using rubocop gem

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,29 +1,21 @@
-require 'pry-byebug'
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
+require "pry-byebug"
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 def count_em(str, substr)
-    str.scan(/(?=#{substr})/).count
-  end
+  str.scan(/(?=#{substr})/).count
+end
 
 def substrings(string, collection)
-    string = string.downcase
-    new_string = ""
-    new_collection = []
-    collection.each do |current_item|
-        n = count_em(string, current_item)
-        if string[current_item] 
-           n.times {new_collection.push(current_item)} 
-        end
-    end
-    new_collection = new_collection.reduce(Hash.new(0)) do |result, word|
-        result[word] += 1
-        result
-      end
-    p new_collection
-
-    
+  string = string.downcase
+  new_collection = []
+  collection.each do |current_item|
+    n = count_em(string, current_item)
+    n.times { new_collection.push(current_item) } if string[current_item]
+  end
+  new_collection = new_collection.each_with_object(Hash.new(0)) do |word, result|
+    result[word] += 1
+  end
+  p new_collection
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
-
