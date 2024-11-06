cef0dcb9d800d4ace4cfc7951de4541744cc4973
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-11-06 15:44:05 -0800

    Add check answer method
    This method will get the answer given by the human user and compare that
    solution with its own code, store the feedback and at the end give it to
    the user. feedback isa hint based on the given answer.
    
    The red pin will be returned when a colored piece is part of the code
    and is positioned exactly the same as in the code. The white pin will
    be returned when the colored piece is part of the code, but is in a
    different position or index

--- master_mind/lib/computer_player.rb
+++ master_mind/lib/computer_player.rb
@@ -13,14 +13,25 @@ class ComputerPlayer < Player
       code << color if code.count(color) < 2
     end
   end
+
+  def check_code(solution)
+    white_pin = 0
+    red_pin = 0
+    solution.each_with_index do |color, index|
+      if code.include?(color)
+        white_pin += 1 if code.index(color) != index
+        red_pin += 1 if code.index(color) == index
+      end
+    end
+    clues = ["white_pin: #{white_pin}", "red_pin: #{red_pin}"]
+    p solution 
+    p clues
+  end
   
 end
 
 comp = ComputerPlayer.new
 comp.set_code
 p comp.code
-10.times do 
-  comp.code.clear
-  comp.set_code
-  p comp.code
-end
+code = ["blue", "red", "black", "white"]
+comp.check_code(code)
