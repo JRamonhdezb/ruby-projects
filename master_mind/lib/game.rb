d4635db1f8583c7a8e68ef6771428fbdfe2f067e
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-11-08 15:44:47 -0800

    Configure logic within the Game class file
    This Game class file requires the ComputerPlayer and HumanPlayer class
    files. The creation of instances of the previous two classes will be
    stored within instance variables in the Game class. The variables are to
    reference the encoder object and the code guesser. Additionally, the
    method that starts and controls the flow of the game was added.

--- master_mind/lib/game.rb
+++ master_mind/lib/game.rb
@@ -1,6 +1,37 @@
+require_relative 'human_player'
+require_relative 'computer_player'
 class Game
+attr_reader :rounds_number, :guesser, :coder
+  def initialize
+    @guesser = HumanPlayer.new
+    @coder = ComputerPlayer.new
+    @rounds_number = 12
+  end
 
-  
-
+  def start
+    winner = "Coder"
+    round = 1
+    puts "Welcome to the game"
+    puts "Computer set code to guess"
+    coder.set_code
+    loop do 
+      break if round > rounds_number
+      puts "Round number #{round}/#{rounds_number}"
+      guesser.set_guess
+      guess = guesser.guess
+      red_pin = coder.check_code(guess)
+      p coder.code
+      if red_pin == 4
+        winner = "Guesser"
+        break
+      end
+      guesser.guess.clear
+      round += 1
+    end
+    puts winner
+  end
 
 end
+
+new_game = Game.new
+new_game.start
