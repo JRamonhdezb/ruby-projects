0466fd4ec3fafbe5b4ede4d961f54d7f4ef879a1
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-11-07 17:17:10 -0800

    Add set guess code
    This method asks the user to enter an input, this input must be a string
    that is within the COLORS constants. The human player will be asked to
    enter four colors, and in addition a color can only be present a maximum
     of two times in the guessing code.

--- master_mind/lib/human_player.rb
+++ master_mind/lib/human_player.rb
@@ -1,4 +1,42 @@
 require_relative 'player'
-class HumanPlayer
+class HumanPlayer < Player
+  attr_accessor :guess
+
+  def initialize
+    @guess = []
+  end
+
+  def set_guess
+    position = 1
+    puts "Colors: #{Player::COLORS}"
+    while guess.length < 4 
+      puts "Please enter a color for your guess code in position ##{position}"
+      color = get_valid_color
+      next if is_twice?(color)
+      self.guess << color
+      puts "Guess code: #{guess}"
+      position += 1
+    end
+    puts "Guess code selected: #{guess}"
+  end
+
+  def get_valid_color
+    color = gets.chomp.downcase
+    while COLORS.none? { |color_piece| color_piece == color}
+      puts "Please enter a valid color. Valid colors: #{Player::COLORS} "
+      color = gets.chomp.downcase
+    end
+    color
+  end
+
+  def is_twice? (color)
+    if guess.count(color) >= 2
+      puts "Can only enter the same color two times in guess code. Try again"
+      return true
+    end
+  end
   
 end
+
+player_1 = HumanPlayer.new
+player_1.set_guess
