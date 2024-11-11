305f2fbc7b1385d46ef0d7ba7c0139ecee9b97ef
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-11-11 15:27:20 -0800

    Improve terminal line outputs
    Changed some output strings to improve user experience. In addition, two
    methods were added, the game_over method? which finishes executing the
    loop when the player guesses the code. The other method is print_result,
    as the name says it will print the game result based on the @winner
    instance variable

--- master_mind/lib/game.rb
+++ master_mind/lib/game.rb
@@ -2,33 +2,47 @@ require_relative 'human_player'
 require_relative 'computer_player'
 class Game
 attr_reader :rounds_number, :guesser, :coder
+attr_accessor :winner
+COLORS =["red", "blue", "yellow", "green", "white", "black"]
+
   def initialize
     @guesser = HumanPlayer.new
     @coder = ComputerPlayer.new
     @rounds_number = 12
+    @winner = nil
   end
 
   def start
-    winner = "Coder"
     round = 1
-    puts "Welcome to the game"
-    puts "Computer set code to guess"
+    puts "Welcome to Mastermind game!, Computer set code to guess"
     coder.set_code
     loop do 
       break if round > rounds_number
       puts "Round number #{round}/#{rounds_number}"
+      puts "Colors to choose: #{COLORS}"
       guesser.set_guess
       guess = guesser.guess
-      red_pin = coder.check_code(guess)
-      p coder.code
-      if red_pin == 4
-        winner = "Guesser"
-        break
-      end
+      result = coder.check_code(guess)
+      break if game_over?(result)
       guesser.guess.clear
       round += 1
     end
-    puts winner
+    print_result
+  end
+
+  def game_over?(result)
+      if result == 4
+        self.winner = guesser
+      end
+  end
+
+  def print_result
+      if winner != nil
+        puts "Congratulations, you win the game."
+      else 
+        puts "Sorry, game over you lose"  
+      end  
+      puts "Code to guess: #{coder.code}"
   end
 
 end
