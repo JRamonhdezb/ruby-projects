2a866df08951252812f56bdd1f7a022050874c22
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-11-12 17:16:19 -0800

    Add set_code and check_method methods
    Now that the user can choose to be the creator of the code, the
    necessary instance methods must be created for the Game class code flow
    to work as it should. Added two methods, the first set_code simply asks
    the user to enter 4 colors respecting the rules. The other method is
    check_code, which receives the guess generated by the computer and the
    user must enter the corresponding pins.

--- master_mind/lib/human_player.rb
+++ master_mind/lib/human_player.rb
@@ -1,9 +1,10 @@
 require_relative 'player'
 class HumanPlayer < Player
-  attr_accessor :guess
+  attr_accessor :guess, :code
 
   def initialize
     @guess = []
+    @code = []
   end
 
   def set_guess
@@ -11,7 +12,7 @@ class HumanPlayer < Player
     while guess.length < 4 
       puts "Please enter a color for your guess code in position ##{position}"
       color = get_valid_color
-      next if is_twice?(color)
+      next if is_twice?(color, guess)
       self.guess << color
       position += 1
     end
@@ -22,17 +23,42 @@ class HumanPlayer < Player
     color = gets.chomp.downcase.strip
     while COLORS.none? { |color_piece| color_piece == color}
       puts "Please enter a valid color. Valid colors: #{Player::COLORS} "
-      color = gets.chomp.downcase
+      color = gets.chomp.downcase.strip
     end
     color
   end
 
-  def is_twice? (color)
-    if guess.count(color) >= 2
-      puts "Can only enter the same color two times in guess code. Try again"
+  def is_twice? (color, set)
+    if set.count(color) >= 2
+      puts "Can only enter the same color two times in array code. Try again"
       return true
     end
   end
-  
+
+  def set_code
+    puts "Available colors to set sectret code: #{Player::COLORS}"
+    while code.length < 4
+      puts "Enter a color for your secret code"
+      color = get_valid_color
+      next if is_twice?(color, code)
+      self.code << color
+    end
+  end
+
+  def check_code(guess)
+    puts "|| Secret Code: #{code} || \n|| Computer Guess: #{guess} ||"
+    puts "Enter number of white pins:"
+    white_pin = enter_number
+    puts "Enter number of red pins:"
+    red_pin = enter_number
+    red_pin
+  end
+
+  def enter_number
+      number = gets.chomp.to_i
+      number
+  end
+
+
 end
 
