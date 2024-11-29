e280bc109133ed5f5b1b1aa9fd022d3c52e7d7e0
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-11-28 16:39:13 -0800

    Add method to get guesses from user player
    The give_guess method simply asks the user to enter a letter. To
    determine if the letter is the expected input, the check_guess method
    was defined, which checks if the letter is part of the alphabet and if
    it has not been previously entered.

---
+++ hang_man/lib/player.rb
@@ -0,0 +1,35 @@
+class Player
+  attr_accessor :guessed_letters, :wrong_guesses
+  
+  Alphabet = %{abcdefghijklmnopqrstuvwxyz}
+
+  def initialize
+    @guessed_letters = []
+    @wrong_guesses = []
+  end
+  
+  def give_guess 
+    puts "Please choose a letter that you think is part of the word to guess"
+    letter = nil
+    loop do 
+      letter = gets.chomp
+      break if check_guess(letter) == true
+    end
+    letter
+  end
+
+  def check_guess(letter)
+    if Alphabet.include? letter
+      if self.guessed_letters.none? {|l| l == letter} && self.wrong_guesses.none? {|l| l == letter}
+        return true
+      else
+        puts "Please use a letter that has no be used in previous tries"
+        return false 
+      end
+    else 
+      puts "Please use a give a valid letter"
+      return false
+    end
+  end
+
+end
