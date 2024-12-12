c8b75302df84cf1dae733a5ac20af724ccde6714
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-12-12 12:59:51 -0800

    Add method of reviewing given letter
    This method iterates over the string secret_word and evaluates if the
    given letter matches the character of the iteration. If the equality is
    true, it will add the letter to the player variable guessed_letters and
    the letter at that index will also be changed in the var secret_code. If
    the letter does not belong to the string, it will be added to the player
    variable wrong_guesses

--- hang_man/lib/game.rb
+++ hang_man/lib/game.rb
@@ -1,17 +1,20 @@
+require_relative 'player'
+
 class Game
-  attr_accessor :secret_word, :secret_code, :tries
+  attr_accessor :secret_word, :secret_code, :tries, :player
 
   def initialize
     @secret_word = pick_random_word
     @secret_code = generate_code
     @tries = 8
+    @player = Player.new
   end
 
   def pick_random_word
     random_word = nil
     dictionary = File.readlines('google-10000-english-no-swears.txt')
     loop do 
-      random_word = dictionary.sample
+      random_word = dictionary.sample.chomp
       break if (random_word.length >= 5 && random_word.length <= 12)
     end
     return random_word
@@ -22,12 +25,20 @@ class Game
     Array.new(size, "_")
   end
 
+  def check_guess(letter)
+    word = secret_word.chars
+    if word.any? {|l| l == letter} == true
+      word.each_with_index do |char, index|
+        if char == letter
+          self.secret_code[index] = char
+          self.player.guessed_letters << char
+        end
+      end
+    else
+      self.player.wrong_guesses << letter 
+    end
+  end
+
 end
 
 
-puts File.exist? "google-10000-english-no-swears.txt"
-game = Game.new
-puts game.secret_word
-puts game.secret_word.length
-puts "Word to guess: #{game.secret_code.join(" ")} "
-puts game.tries
